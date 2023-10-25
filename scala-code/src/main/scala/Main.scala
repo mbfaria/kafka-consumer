3d3d4eaf647903e868b56ab1973da025061654b9
Matheus <matheus.brito000@gmail.com>
2023-10-25 14:01:31 -0300

    feat: add udf example

--- scala-code/src/main/scala/Main.scala
+++ scala-code/src/main/scala/Main.scala
@@ -3,13 +3,23 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.{col, explode, max, when, concat, lit}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.udf
+
 
 class MyScalaClass(sqlContext: SQLContext, df: DataFrame) {
   import sqlContext.implicits._
   import org.apache.spark.sql.functions._
 
-
   def myScalaFunction(column: String): DataFrame = {
     df.withColumn(column, concat(col(column), lit(" was processed by Scala function.")))
+  }
+}
+
+object MyScalaObject {
+  def myScalaUDF(): UserDefinedFunction = {
+    udf((value: Double) => {
+      value * 2
+    })
   }
 }
